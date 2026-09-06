2db83fab88a2091d017bae0626fbe01c19bcf135
Nathan Timothy <03nart@gmail.com>
2026-09-06 15:28:19 +0100

    Remove obsolete KitAgent stat component

--- src/components/StatCard.jsx
+++
@@ -1,2 +0,0 @@
-import React from 'react';
-export default function StatCard({ label, value, detail }) { return <div className="stat"><span>{label}</span><b>{value}</b>{detail && <small>{detail}</small>}</div>; }
