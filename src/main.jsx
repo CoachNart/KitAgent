43732d59767a4ae4495252c3d40cf76d0a91c5ec
Nathan Timothy <03nart@gmail.com>
2026-09-06 16:38:01 +0100

    Connect KitAgent UI to real authentication gate

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import AuthGate from './AuthGate.jsx';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><App /></React.StrictMode>
+  <React.StrictMode>
+    <AuthGate><App /></AuthGate>
+  </React.StrictMode>
 );
