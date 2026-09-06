de2b17a91902430894ee62fff8f5b094f1b0483f
Nathan Timothy <03nart@gmail.com>
2026-09-06 18:57:14 +0100

    Load final KitAgent visual overrides

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx';
 import AuthGate from './AuthGate.jsx';
 import './styles.css';
 import './brand.css';
+import './overrides.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
