1466afe2e373dbddb0ea309e51e28425e083b9e3
Nathan Timothy <03nart@gmail.com>
2026-09-07 13:26:48 +0100

    Restore original Command Center terminal

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import AuthGate from './AuthGate.jsx';
-import AgentTerminalLayer from './AgentTerminalLayer.jsx';
 import './styles.css';
 import './brand.css';
 import './overrides.css';
 import './command-center-overrides.css';
-import './command-center-final.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthGate>{user => <><App user={user} /><AgentTerminalLayer /></>}</AuthGate>
+    <AuthGate>{user => <App user={user} />}</AuthGate>
   </React.StrictMode>
 );
