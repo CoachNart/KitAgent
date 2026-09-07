8c157107691caf40d0731a980bb75e666596587d
Nathan Timothy <03nart@gmail.com>
2026-09-07 11:20:10 +0100

    Register Morpho execution adapter

--- src/adapters/index.js
+++ src/adapters/index.js
@@ -4,12 +4,14 @@ import { tokenAdapter } from './erc20.js';
 import { uniswapV2Adapter } from './uniswapV2.js';
 import { nftAdapter } from './erc721.js';
 import { discoveryAdapter } from './discovery.js';
+import { morphoAdapter } from './morpho.js';
 
 export const adapterRegistry = new AdapterRegistry([
   nativeTransferAdapter,
   tokenAdapter,
   uniswapV2Adapter,
   nftAdapter,
+  morphoAdapter,
   discoveryAdapter,
 ]);
 
