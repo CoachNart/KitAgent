18c1f935ea8fa250d012bd71ab8083e00510ddc1
Nathan Timothy <03nart@gmail.com>
2026-09-07 15:38:35 +0100

    Use primary Command Center instead of bridge overlay

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import AuthGate from './AuthGate.jsx';
-import CommandAgentBridge from './CommandAgentBridge.jsx';
 import './styles.css';
 import './brand.css';
 import './overrides.css';
@@ -11,6 +10,6 @@ import './agent-terminal.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthGate>{user => <CommandAgentBridge><App user={user} /></CommandAgentBridge>}</AuthGate>
+    <AuthGate>{user => <App user={user} />}</AuthGate>
   </React.StrictMode>
 );
