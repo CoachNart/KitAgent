1df40d5bf4d039b82bd42171362e1778afee3755
Nathan Timothy <03nart@gmail.com>
2026-09-07 10:05:22 +0100

    Register discovery adapters

--- src/adapters/index.js
+++ src/adapters/index.js
@@ -3,12 +3,14 @@ import { nativeTransferAdapter } from './nativeEth.js';
 import { tokenAdapter } from './erc20.js';
 import { uniswapV2Adapter } from './uniswapV2.js';
 import { nftAdapter } from './erc721.js';
+import { discoveryAdapter } from './discovery.js';
 
 export const adapterRegistry = new AdapterRegistry([
   nativeTransferAdapter,
   tokenAdapter,
   uniswapV2Adapter,
   nftAdapter,
+  discoveryAdapter,
 ]);
 
 export const getAdapterCapabilities = () => adapterRegistry.list().map(({ id, name, capabilities, chains }) => ({ id, name, capabilities, chains }));
