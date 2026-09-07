7deed241d542f2a385444871ad54e327b43a54ee
Nathan Timothy <03nart@gmail.com>
2026-09-07 13:08:43 +0100

    feat: make agent execution terminal the primary task output interface

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import AuthGate from './AuthGate.jsx';
+import AgentTerminalLayer from './AgentTerminalLayer.jsx';
 import './styles.css';
 import './brand.css';
 import './overrides.css';
@@ -10,6 +11,6 @@ import './command-center-final.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthGate>{user => <App user={user} />}</AuthGate>
+    <AuthGate>{user => <><App user={user} /><AgentTerminalLayer /></>}</AuthGate>
   </React.StrictMode>
 );
