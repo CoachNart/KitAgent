65ceaca44bb1716e3feb7f01cd19aa68b10c175f
Nathan Timothy <03nart@gmail.com>
2026-09-07 14:21:22 +0100

    Wire Command Center into live agent bridge

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import AuthGate from './AuthGate.jsx';
+import CommandAgentBridge from './CommandAgentBridge.jsx';
 import './styles.css';
 import './brand.css';
 import './overrides.css';
 import './command-center-overrides.css';
+import './agent-terminal.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthGate>{user => <App user={user} />}</AuthGate>
+    <AuthGate>{user => <CommandAgentBridge><App user={user} /></CommandAgentBridge>}</AuthGate>
   </React.StrictMode>
 );
