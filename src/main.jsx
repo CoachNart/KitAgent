01152d2d98ed877ffa171ffd3fd74dd60792e804
Nathan Timothy <03nart@gmail.com>
2026-09-12 01:30:49 +0100

    feat: load MEXC data layer on CEX terminal

--- src/main.jsx
+++ src/main.jsx
@@ -20,6 +20,7 @@ import './contentProtection.js';
 import './cexIntegratedEnhancements.js';
 import './cexTerminal.js';
 import './cexStableInteractionPatch.js';
+import './cexMexcDataPatch.js';
 import { startMarketAlerts } from './marketAlerts.js';
 
 startMarketAlerts();
